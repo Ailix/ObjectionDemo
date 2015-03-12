70bf56b764da4269be36b80c2577c08194758b38
Ailix <Ailix2018@gmail.com>
2015-03-12 11:13:10 +0800

    Initial Commit

---
+++ ObjectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectionDemo
+//
+//  Created by Ailix on 15/3/12.
+//  Copyright (c) 2015年 Ailix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectionDemo
+//
+//  Created by Ailix on 15/3/12.
+//  Copyright (c) 2015年 Ailix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
